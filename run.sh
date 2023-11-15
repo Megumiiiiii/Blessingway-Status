b3de03ea0ab0e11e9ed047a833b8507c493d03ff
megumii <contact@megumii.xyz>
2023-11-15 16:30:35 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 while true; do
     response=$(curl -k -f https://blessingway.xyz 2>&1)
-    if [[ $response == *"502 Bad Gateway"* ]]; then
-        echo "Received Bad Gateway error. Retrying in 60 seconds..."
-        sleep 60
+    if [[ $response == *"502"* ]]; then
+        echo "Received Bad Gateway error. Retrying in 30 seconds..."
+        sleep 30
     else
         echo "Request successful."
         # Do something with the response (e.g., update the file)
