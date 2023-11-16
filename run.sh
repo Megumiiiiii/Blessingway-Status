73ac848afc0acff52a7039b4c33d204651ab6b3e
megumii <contact@megumii.xyz>
2023-11-17 05:25:23 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 curl_output=$(curl -f https://blessingway.xyz 2>&1)
 
 # Menyimpan hasil output curl ke dalam file Update.tsx
-echo "$curl_output" > Update.tsx
+echo "$curl_output" >> Update.tsx
 
 # Menambahkan pesan bahwa Update.tsx telah diperbarui
 echo "Check Update.tsx." 
