4089c6c4b8c9458dcadd4a1157729dc7358f45dc
megumii <contact@megumii.xyz>
2023-11-17 05:25:10 +0700

    Update r.sh

--- r.sh
+++ r.sh
@@ -4,7 +4,7 @@
 curl_output=$(curl -f https://blessingway.xyz 2>&1)
 
 # Menyimpan hasil output curl ke dalam file Update.tsx
-echo "$curl_output" > Update.mdx
+echo "$curl_output" >> Update.mdx
 
 # Menambahkan pesan bahwa Update.tsx telah diperbarui
 echo "Check Update.mdx." 
