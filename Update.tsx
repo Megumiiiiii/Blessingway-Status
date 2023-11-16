519c08eb1485c90dff9c69e80bc7ba203ad05c66
Megumiiiiii <contact@megumii.xyz>
2023-11-16 06:47:04 +0000

    🌄 auto commit

--- Update.tsx
+++ Update.tsx
@@ -459,3 +459,7 @@
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100   213  100   213    0     0    560      0 --:--:-- --:--:-- --:--:--   562
 {"network":"arweave.N.1","version":5,"release":66,"height":1303374,"current":"yq85f0xrlBN3nRqXn-hMmtQf2O6XDS82KcamxRzLyt7gexoXIss8KqxnMCvoXAp5","blocks":1303375,"peers":106,"queue_length":0,"node_state_latency":0}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100   213  100   213    0     0    342      0 --:--:-- --:--:-- --:--:--   342
+{"network":"arweave.N.1","version":5,"release":66,"height":1303383,"current":"HhL3sq8R2dMjKpEtChjw2JqF2WaWvyGo9Jv4yxHZElg1BbBuYslDvwuCliPw4sCZ","blocks":1303384,"peers":106,"queue_length":0,"node_state_latency":1}
