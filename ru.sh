c82108e2ce1b293049aeccf15131e4be05c63b54
megumii <contact@megumii.xyz>
2023-11-17 05:09:41 +0700

    Create ru.sh

---
+++ ru.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Menjalankan perintah curl dan menyimpan outputnya ke dalam file Update.tsx
+curl_output=$(curl -f https://blessingway.xyz 2>&1)
+
+# Menyimpan hasil output curl ke dalam file Update.tsx
+echo "$curl_output" > Update.jsx
+
+# Menambahkan pesan bahwa Update.tsx telah diperbarui
+echo "Check Update.jsx." 
