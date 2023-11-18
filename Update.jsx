9ea656866055c49bf09e681a4c9f982f7ff4151c
Megumiiiiii <contact@megumii.xyz>
2023-11-18 08:31:41 +0000

    🔱 auto commit

--- Update.jsx
+++ Update.jsx
@@ -358,3 +358,7 @@
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100   213  100   213    0     0    322      0 --:--:-- --:--:-- --:--:--   322
 {"network":"arweave.N.1","version":5,"release":66,"height":1304772,"current":"1uB58rpb8N8rQ-ZSXTjQSrN19J47ILyzQP0s-7X4l-1muOmObzLvmHJKx9pNT9qr","blocks":1304773,"peers":110,"queue_length":0,"node_state_latency":1}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100   213  100   213    0     0    405      0 --:--:-- --:--:-- --:--:--   405
+{"network":"arweave.N.1","version":5,"release":66,"height":1304785,"current":"msB6vOFh1d9lRjkCWNsqrXxQgw_lzML41g8hfvLt4FAt2ENEZoSIYM29L2twauTJ","blocks":1304786,"peers":109,"queue_length":0,"node_state_latency":0}
--- Update.tsx
+++ Update.tsx
@@ -358,3 +358,7 @@
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100   213  100   213    0     0    315      0 --:--:-- --:--:-- --:--:--   316
 {"network":"arweave.N.1","version":5,"release":66,"height":1304773,"current":"rgrGV-DKM1qiBtw_gZi7CggT4SW3v2-3G6N6y2t4wh0PRaGamr0h-k4umPzvPGC1","blocks":1304774,"peers":109,"queue_length":0,"node_state_latency":0}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100   213  100   213    0     0    309      0 --:--:-- --:--:-- --:--:--   309
+{"network":"arweave.N.1","version":5,"release":66,"height":1304785,"current":"msB6vOFh1d9lRjkCWNsqrXxQgw_lzML41g8hfvLt4FAt2ENEZoSIYM29L2twauTJ","blocks":1304786,"peers":109,"queue_length":0,"node_state_latency":0}
