b78f389addf0b407a57f888de0f72afbcad2eeb5
megumii <contact@megumii.xyz>
2023-11-20 17:26:31 +0700

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Menjalankan perintah curl dan menyimpan outputnya ke dalam file Update.tsx
-curl_output=$(curl -f https://blessingway.xyz 2>&1)
+curl_output=$(curl -f https://blessingway.xyz/ar-io/healthcheck 2>&1)
 
 # Menyimpan hasil output curl ke dalam file Update.tsx
 echo "$curl_output" >> Update.mdx
