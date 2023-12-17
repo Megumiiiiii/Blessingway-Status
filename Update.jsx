9b87efaaa63a57a761013e4a76c70274a65de019
Megumiiiiii <contact@megumii.xyz>
2023-12-17 11:13:42 +0000

    🌄 auto commit

--- Update.jsx
+++ Update.jsx
@@ -7882,3 +7882,7 @@ curl: (22) The requested URL returned error: 503
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    94  100    94    0     0    162      0 --:--:-- --:--:-- --:--:--   162
 {"uptime":36.192367212,"message":"Welcome to the Permaweb.","date":"2023-12-17T10:46:37.741Z"}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0  0     0    0     0    0     0      0      0 --:--:--  0:00:01 --:--:--     0  0     0    0     0    0     0      0      0 --:--:--  0:00:02 --:--:--     0  0     0    0     0    0     0      0      0 --:--:--  0:00:03 --:--:--     0  0   114    0     0    0     0      0      0 --:--:--  0:00:04 --:--:--     0
+curl: (22) The requested URL returned error: 503
